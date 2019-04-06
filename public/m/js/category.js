15f3ce67ac11669d41fce338ba911c18cb675372
qnlmd <262313632@qq.com>
2019-04-06 09:01:44 +0800

    全端第一天第一次提交

---
+++ public/m/js/category.js
@@ -0,0 +1,9 @@
+//滑动
+mui('.main-left .mui-scroll-wrapper').scroll({
+    deceleration: 0.0005, //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+    indicators: false,
+});
+
+mui('.main-right .mui-scroll-wrapper').scroll({
+	deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+});
