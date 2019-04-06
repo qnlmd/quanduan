d05815ba6785b5702888d9c665de09e3da95dc7d
qnlmd <262313632@qq.com>
2019-04-06 21:50:33 +0800

    全段第二天第一次提交

--- public/m/js/category.js
+++ public/m/js/category.js
@@ -1,9 +1,63 @@
-//滑动
-mui('.main-left .mui-scroll-wrapper').scroll({
-    deceleration: 0.0005, //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
-    indicators: false,
-});
+$(function () {
+    initLeftScroll();
+    initRightScroll();
+    queryTopCategory();
+    querySecondCategory(1);
 
-mui('.main-right .mui-scroll-wrapper').scroll({
-	deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
-});
+    //左侧滑动
+    function initLeftScroll() {
+        mui('.main-left .mui-scroll-wrapper').scroll({
+            deceleration: 0.0005, //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+            indicators: false,
+        });
+    }
+
+    //右侧滑动
+    function initRightScroll() {
+        mui('.main-right .mui-scroll-wrapper').scroll({
+            deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+        });
+    }
+
+
+    // 主体部分左侧的ajax请求
+    function queryTopCategory() {
+        $.ajax({
+            url: '/category/queryTopCategory',
+            success: function (data) {
+                var html = template('mainLeftTpl', data);
+                $('.main-left ul').html(html);
+                liClick();
+            }
+
+        })
+    }
+
+    //主体部分右侧的ajax请求
+    function querySecondCategory(id) {
+        $.ajax({
+            url: '/category/querySecondCategory',
+            data: {
+                id: id
+            },
+            success: function (data) {
+                console.log(data)
+                var html = template('mainRightTpl', data)
+                $('.main-right .mui-row').html(html)
+                
+            }
+        })
+    }
+
+    //点击事件
+    function liClick() {
+        var lis = $('.main-left ul li');
+        console.log(lis);
+        lis.on('click', function () {
+            var id = $(this).data('id');
+            $(this).addClass('active').siblings().removeClass('active');
+            querySecondCategory(id);
+        })
+    }
+
+})
---
+++ public/m/js/search.js
@@ -0,0 +1,78 @@
+$(function(){
+    addHistory();
+    queryHistory();
+    searchScroll();
+    deleteHistory();
+
+    function searchScroll(){
+        mui('.mui-scroll-wrapper').scroll({
+            deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+        });
+    }
+
+
+    //1.添加搜索记录
+    function addHistory(){
+         
+        // var arr = [];  在次打开页面会把数组恢复成空数组
+        var arr = localStorage.getItem('searchNr');
+        // 如果没有值得到的是null,证明是没有记录过local
+        // 如果有值 需要先把字符窜格式更改
+        if(arr == null){
+            arr =[]
+        }else{
+            arr =JSON.parse(arr)
+        }
+        //点击事件
+        $('#search-btn').on('tap',function(){
+            var inputNr = $('#search-imp').val().trim();
+            $('#search-imp').val('');
+            // 输入的是空值
+            if(inputNr == ""){
+                return;
+            }
+
+            //数组去从 
+            for(var i = 0;i<=arr.length;i++){
+                if(arr[i] == inputNr){
+                    // !!把旧的删除
+                    arr.splice(i,1);
+                }
+            };
+            console.log(arr);
+            //添加数组内容 在前面
+            arr.unshift(inputNr);
+            
+            localStorage.setItem('searchNr',JSON.stringify(arr));
+            queryHistory();
+        })
+    }
+
+
+    //2.查询搜索记录
+    function queryHistory(){
+        var arr = localStorage.getItem('searchNr');
+        if(arr == null){
+            arr =[]
+        }else{
+            arr =JSON.parse(arr)
+        }
+        var html = template('seachTpl',{list:arr});
+        $('.search-content ul').html(html);
+    }
+
+
+    //3.删除记录
+    function deleteHistory(){
+        // var arr = localStorage.getItem('searchNr');
+        // if(arr == null){
+        //     arr =[]
+        // }else{
+        //     arr =JSON.parse(arr)
+        // }
+        $('.mui-card-content ul').on('tap','li',function(){
+            
+                 
+        })
+    }
+})
